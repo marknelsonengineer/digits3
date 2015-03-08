aedbedf1889fa7296078501fd976b49b431b0822
Mark Nelson <mr_nelson@icloud.com>
2015-03-08 07:40:15 -1000

    Fixed up tests and package-info pages.

--- test/test/IntegrationTest.java
+++ test/test/IntegrationTest.java
@@ -1,3 +1,12 @@
+/**
+ * Use a simulated browser to exercise the results from page requests.
+ *
+ * @author Mark Nelson
+ * @since 6.0
+ * @see http://junit.org
+ * @see https://www.playframework.com
+ */
+
 package test;
 
 import org.junit.Test;
@@ -11,24 +20,82 @@ import static play.test.Helpers.inMemoryDatabase;
 import static play.test.Helpers.running;
 import static play.test.Helpers.testServer;
 
+
 /**
- * Use a simulated browser to exercise the results from a request to the default page.
+ * Use a simulated browser to exercise the results from page requests.  These tests should focus on content and
+ * page-to-page flow.
  */
 public class IntegrationTest {
 
   /**
-   * Check if the welcome page is being shown.
+   * Check if the home page is shown.
    */
   @Test
-  public void test() {
+  public void testHome() {
     running(testServer(3333, fakeApplication(inMemoryDatabase())), HTMLUNIT, new Callback<TestBrowser>() {
       public void invoke(TestBrowser browser) {
         browser.goTo("http://localhost:3333");
-        assertThat(browser.pageSource()).contains("bootstrap.min.js");
+        assertThat(browser.pageSource()).contains("The home page.");
 
-        //TODO:  Add the other pages
+        browser.goTo("http://localhost:3333/");
+        assertThat(browser.pageSource()).contains("The home page.");
       }
     });
   }
 
+
+  /**
+   * Check if the Page 1 is shown.
+   */
+  @Test
+  public void testPage1() {
+    running(testServer(3333, fakeApplication(inMemoryDatabase())), HTMLUNIT, new Callback<TestBrowser>() {
+      public void invoke(TestBrowser browser) {
+        browser.goTo("http://localhost:3333/page1");
+        assertThat(browser.pageSource()).contains("The first page.");
+      }
+    });
+  }
+
+
+  /**
+   * Check if the Page 2 is shown.
+   */
+  @Test
+  public void testPage2() {
+    running(testServer(3333, fakeApplication(inMemoryDatabase())), HTMLUNIT, new Callback<TestBrowser>() {
+      public void invoke(TestBrowser browser) {
+        browser.goTo("http://localhost:3333/page2");
+        assertThat(browser.pageSource()).contains("The second page.");
+      }
+    });
+  }
+
+
+  /**
+   * Check if the About page is shown.
+   */
+  @Test
+  public void testAbout() {
+    running(testServer(3333, fakeApplication(inMemoryDatabase())), HTMLUNIT, new Callback<TestBrowser>() {
+      public void invoke(TestBrowser browser) {
+        browser.goTo("http://localhost:3333/about");
+        assertThat(browser.pageSource()).contains("About this application...");
+      }
+    });
+  }
+
+
+  /**
+   * Check if the Contact page is shown.
+   */
+  @Test
+  public void testContact() {
+    running(testServer(3333, fakeApplication(inMemoryDatabase())), HTMLUNIT, new Callback<TestBrowser>() {
+      public void invoke(TestBrowser browser) {
+        browser.goTo("http://localhost:3333/contact");
+        assertThat(browser.pageSource()).contains("Please contact");
+      }
+    });
+  }
 }
